ecb7c33809f43949333a5a1f1cae2b32272d0aa4
Manno <manno@xs4all.nl>
2020-11-17 14:38:10 +0100

    Main menu added. Closes #1

---
+++ DemoProject/Assets/Scripts/SceneNavigation.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 
+/// </summary>
+public class SceneNavigation : MonoBehaviour
+{
+
+    /// <summary>
+    /// nice dessciptive text
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
